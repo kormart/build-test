0e962d55dd6d3da11bae7687fffc057294a6f6f4
Martin Körling <martin.korling@gmail.com>
2023-06-26 20:55:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Define a function that prints a message to the console
+function printMessage(message) {
+  console.log(message);
+}
+
+// Call the function with the desired message
+printMessage("Hello Europe!");
